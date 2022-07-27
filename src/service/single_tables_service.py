6f048b19b2d7382e76a721335972b4180c787f98
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-07-27 16:07:16 +0900

    refactor repositories

--- src/service/single_tables_service.py
+++ src/service/single_tables_service.py
@@ -1,25 +1,23 @@
-from src.repository import single_table_sql_methods as repository
+from src.repository import single_table_repository as repository
 
 # query table by table_name
 async def get_single_table(pool, table_name:str):
     if table_name != "data_dict" and table_name != "fdic_fail":
         table_name = "table_" + table_name
-    return await repository.get_all_rows(pool,table_name)
+    return await repository.get_single_table(pool,table_name)
 
 # query describe table_name
-async def describe_single_table_service(pool,table_name):
+async def describe_single_table_service(pool,table_name:str):
     if table_name != "data_dict" and table_name != "fdic_fail":
         table_name = "table_" + table_name
-    query = "describe " + table_name + ";"
-    return await repository.custom_query(pool,query)
+    return await repository.describe_single_table(pool,table_name)
 
 # query showing all tables
 async def get_all_table_names(pool):
-    return await repository.custom_query(pool,"show tables;")
+    return await repository.get_all_table_name(pool)
 
 # query single table length (# of rows)
-async def get_single_table_length(pool, table_name):
+async def get_single_table_length(pool, table_name:str):
     if table_name != "data_dict" and table_name != "fdic_fail":
         table_name = "table_" + table_name
-    query = "SELECT COUNT(*) AS length FROM "+table_name+";"
-    return await repository.custom_query_single(pool,query)
+    return await repository.get_single_table_length(pool,table_name)
