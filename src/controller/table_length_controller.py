4fba0275e63272b4ccaa5d86d41ae76aabb063bb
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-07-28 17:18:11 +0900

    data_dict search

--- src/controller/table_length_controller.py
+++ src/controller/table_length_controller.py
@@ -4,39 +4,6 @@ from src.service import table_length_service as service
 
 router = APIRouter()
 
-@router.get(path="/data_dict")
-async def get_data_dict_length(request:Request,item_code:str="",meaning:str=""):
-    return await service.get_data_dict_length(request.state.pool, item_code, meaning)
-
-@router.get(path="/fdic_fail")
-async def get_data_dict_length(request:Request,FDICCertificateNumber:str="", BankName:str="", City:str="", ST:str="", AcquiringInstitution:str="", startClosingDate:str="1950-01-01",endClosingDate:str="2050-01-01"):
-    print("length")
-    return await service.get_fdic_fail_length(request.state.pool,FDICCertificateNumber, BankName, City, ST, AcquiringInstitution, startClosingDate, endClosingDate)
-
-@router.get(path="/FDICCertificateNumber")
-async def get_FDICCertificateNumber_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"FDICCertificateNumber")
-
-@router.get(path="/FinancialInstitutionFilingType")
-async def get_FDICCertificateNumber_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"FinancialInstitutionFilingType")
-
-@router.get(path="/FinancialInstitutionZipCode")
-async def get_FinancialInstitutionZipCode_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"FinancialInstitutionZipCode")
-
-@router.get(path="/OCCCharterNumber")
-async def get_OCCCharterNumber_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"OCCCharterNumber")
-
-@router.get(path="/OTSDocketNumber")
-async def get_OTSDocketNumber_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"OTSDocketNumber")
-
-@router.get(path="/PrimaryABARoutingNumber")
-async def get_PrimaryABARoutingNumber_length(request:Request):
-    return await service.get_single_table_length(request.state.pool,"PrimaryABARoutingNumber")
-
 @router.get(path="/{table_name}")
 async def get_table_length(request:Request,table_name:str):
   return await service.get_single_table_length(request.state.pool,table_name)
--- src/repository/table_length_repository.py
+++ src/repository/table_length_repository.py
@@ -1,15 +1,5 @@
 from src.repository import sql_methods as sql
 
-async def get_data_dict_length(pool):
-    query = "SELECT COUNT(*) AS length FROM data_dict " 
-    print(query)
-    return await sql.get_single_row(pool,query)
-
-async def get_fdic_fail_length(pool):
-    query = "SELECT COUNT(*) AS length FROM fdic_fail "
-    print(query) 
-    return await sql.get_single_row(pool,query)
-
 async def get_single_table_length(pool, table_name:str):
     query = "SELECT COUNT(*) AS length FROM " + table_name +";"
     return await sql.get_single_row(pool,query)
